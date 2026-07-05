f8e9835a4562d66a4bada7722929306230214b48
Max Beket <maxim.beketov@gmail.com>
2026-07-05 14:30:49 +0300

    rewrite

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import flet as ft
 from replace import replace
 
+class mwb:
+    def __new__(cls,message):
+       instance = super().__new__(cls)
+       instance.message = replace(message) 
+       return ft.Row(controls=[ft.TextField(multiline=True,value=instance.message),
+       ft.Button("Copy",on_click=instance.set_to_clipboard)])               
+    
+    async def set_to_clipboard(self):
+        await ft.Clipboard().set(self.message)
+    
 def main(page: ft.Page):
     chat = ft.Column()
     new_message = ft.TextField(multiline=True)
-    text_to_copy = ft.TextField(multiline=True)
-
 
     def send_click(e):
-        chat.controls.append(ft.Row(controls=[ft.Text(replace(new_message.value)),ft.Button("Copy", on_click=set_to_clipboard)]))
+        chat.controls.append(mwb(new_message.value))
         new_message.value = ""
-        text_to_copy.value = ""
 
-    async def set_to_clipboard():
-        await ft.Clipboard().set(text_to_copy.value)
-        text_to_copy.value = ""
-        page.show_dialog(ft.SnackBar("Text copied to clipboard"))
-        
     page.add(
         chat,
         ft.Row(controls=[new_message, ft.Button("Convert", on_click=send_click)]),
     )
-
 
 ft.run(main)
