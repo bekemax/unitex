931b171a3244028a1e6d794348b37efd1b6c010a
Max(im) Beketov <32070551+bekemax@users.noreply.github.com>
2026-07-06 22:38:34 +0300

    up

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 import flet as ft
 from replace import replace
 
-class mwb:
-    def __new__(cls,message):
-       instance = super().__new__(cls)
-       instance.message = replace(message) 
-       return ft.Row(controls=[ft.TextField(multiline=True,value=instance.message),
-       ft.Button("Copy",on_click=instance.set_to_clipboard)])               
-    
-    async def set_to_clipboard(self):
-        await ft.Clipboard().set(self.message)
+
     
 def main(page: ft.Page):
-    chat = ft.Column()
-    new_message = ft.TextField(multiline=True)
+    
+          
+  tf1 = ft.TextField(label="Type here",multiline=True,max_lines=5)  
+  
+  tf2 = ft.TextField(label="Result",multiline=True,max_lines=5)
 
-    def send_click(e):
-        chat.controls.append(mwb(new_message.value))
-        new_message.value = ""
+  async def set_to_clipboard(e):
+        await ft.Clipboard().set(tf2.value)
+  
+  def on_click1(e):
+      tf2.value = replace(tf1.value) 
+      
+  but1 = ft.Button("Convert",on_click=on_click1)
 
-    page.add(
-        chat,
-        ft.Row(controls=[new_message, ft.Button("Convert", on_click=send_click)]),
-    )
+  but2 = ft.Button("Copy",on_click=set_to_clipboard)  
+  
+  buttons = ft.Row(expand=True,controls=[but1,but2])
+
+  col = ft.Column(spacing=10, controls=[tf1,tf2,buttons],
+  alignment=ft.CrossAxisAlignment.CENTER)
+    
+
+  page.window.height=700
+  page.window.width=400      
+  page.window.resizeable=False
+  
+  page.add(col)  
 
 ft.run(main)
